75aa8d9bc014fd5d64e6af8d8fb8cec6642ca735
Abdulrhaman Alzahrani <alzahrani.abdulrahman3@gmail.com>
2024-04-26 15:40:08 +0300

    Cache-data

--- index.js
+++ index.js
@@ -26,19 +26,39 @@ const limiter = rateLimit({
   message: "Too many request",
 });
 
+// slow down requests
 const slow_down = slowDown({
   windowMs: 30 * 1000,
   delayAfter: 5, // allow 5 requests per 30 seconds
   delayMs: (hits) => hits * 500, // Add 500ms of delay to every request after the 5th one.
 });
 
+// cache responses
+let cache_data;
+let cache_time;
+
 app.get("/request-limiter", limiter, (req, res) =>
-  res.send("Hello from a rate limitted app.")
+  res.send("This is slow rate-limit endpoint")
 );
 
 app.get("/slow-down", slow_down, (req, res) =>
-  res.send("This is an open endpoint")
+  res.send("This is slow-down endpoint")
 );
+
+app.get("/cache-data", (req, res) => {
+  // set cache time 30 seconds
+  if (cache_time && cache_time > Date.now() - 30 * 1000) {
+    return res.json({
+      message: cache_data + " cached",
+    });
+  }
+  cache_time = Date.now();
+  cache_data = "This is slow cache-data endpoint";
+  return res.json({
+    message: "This is cached-data endpoint",
+    cacheTime: cache_time,
+  });
+});
 
 app.listen(3000, () => {
   console.log("🚀 server on port 3000 🔐");
